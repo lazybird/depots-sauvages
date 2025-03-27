cdf9485ff4e02b5c3446ced53cf67c570e5b9fcf
Sylvain <lazybird@users.noreply.github.com>
2025-03-27 14:47:29 +0100

    Change static root for prod

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -12,3 +12,5 @@ SECRET_KEY = env("SECRET_KEY")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 DATABASES = {"default": env.db("DATABASE_URL")}
+
+STATIC_ROOT = env.str("STATIC_ROOT", default="dist/static")
